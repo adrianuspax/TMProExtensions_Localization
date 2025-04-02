a6286e046f453a5d75a23a5b448fc802dbeb6981
Adriano Paz <contato@adrianopaz.com>
2025-04-02 17:24:42 +0900

    Logic improvement

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,4 @@
 # 1.0.2 - LocalizationManager.SetLocalizationString with return operation
 # 1.0.3 - Localization Manager access to Operation Handler
 # 2.0.1 - Updated for ASPax
+# 3.0.0 - Logic improvement
--- Runtime/Scripts/LocalizationManager.cs
+++ Runtime/Scripts/LocalizationManager.cs
@@ -5,10 +5,18 @@ using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace ASPax.Extensions
 {
+    /// <summary>
+    /// Localization Manager
+    /// </summary>
     public static class LocalizationManager
     {
-        private static AsyncOperationHandle<string> _operation;
-
+        /// <summary>
+        /// Set localization string
+        /// </summary>
+        /// <param name="entry">Entry value from table</param>
+        /// <param name="table">Table Value</param>
+        /// <param name="action">Function to set action</param>
+        /// <returns>Return Async operation Handle</returns>
         public static AsyncOperationHandle<string> SetLocalizationString(string entry, string table, UnityAction<string> action)
         {
             if (string.IsNullOrEmpty(table))
@@ -23,27 +31,27 @@ namespace ASPax.Extensions
                 return default;
             }
 
-            LocalizedStringDatabase stringDatabase = LocalizationSettings.StringDatabase;
-            _operation = stringDatabase.GetLocalizedStringAsync(table, entry);
+            var operation = LocalizationSettings.StringDatabase.GetLocalizedStringAsync(table, entry);
+            SetAsyncOperationHandle(operation, (operation) => action?.Invoke(operation.Result));
+            return operation;
+        }
+        /// <summary>
+        /// Set Async Operation Handle
+        /// </summary>
+        /// <typeparam name="T">Type Generic T</typeparam>
+        /// <param name="operation">The Async Operation Handle</param>
+        /// <param name="action">Function to set action</param>
+        public static void SetAsyncOperationHandle<T>(AsyncOperationHandle<T> operation, UnityAction<AsyncOperationHandle<T>> action)
+        {
+            _update(operation);
 
-            _updateString(_operation);
-
-            return _operation;
-
-            void _updateString(AsyncOperationHandle<string> operation)
+            void _update(AsyncOperationHandle<T> operation)
             {
-                if (!operation.IsDone)
-                {
-                    operation.Completed += _updateString;
-                    return;
-                }
-
-                action?.Invoke(operation.Result);
+                if (operation.IsDone)
+                    action?.Invoke(operation);
+                else
+                    operation.Completed += _update;
             }
         }
-
-        public static AsyncOperationHandle<string> AsyncOperationHandle => _operation;
     }
 }
-
-
